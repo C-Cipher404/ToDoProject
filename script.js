df66133fa11f98ffe6d4cd57704380eb83468530
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2024-10-14 09:10:35 -0600

    adding more CSS and more HTML and more JS

--- script.js
+++ script.js
@@ -1,76 +1,93 @@
-document.getElementById("addButton").addEventListener("click", function() {
-    const todoInput = document.getElementById("todoInput");
-    const todoText = todoInput.value.trim(); 
-    
-    if (todoText) {
-        currentList.toDos.push({ task: todoText, completed: false });
-        todoInput.value = "";
-        render();
-    }
-});
-
 const lists = {
-    1: { name: 'Shopping list', items: ['milk', 'bread', 'eggs'] },
-    2: { name: 'Honey do list', items: ['clean garage', 'mow lawn'] },
-    3: { name: 'Workout routine', items: ['push-ups', 'squats', 'planks'] }
+    1: { name: 'Shopping list', toDos: [] },
+    2: { name: 'Honey do list', toDos: [] },
+    3: { name: 'Workout routine', toDos: [] },
 };
 
-const currentList = {
-    name: "Shopping list",
-    toDos: [
-        { task: "Buy milk", completed: false },
-        { task: "Buy eggs", completed: true },
-        { task: "Buy bread", completed: false }
-    ]
-};
+let currentList = lists[1]; 
+
+const todoInput = document.getElementById("todoInput");
+const addButton = document.getElementById("addButton");
+const listsContainer = document.getElementById('lists');
+const currentListName = document.getElementById('current-list-name');
+const currentListToDos = document.getElementById('current-list-toDos');
+
+addButton.addEventListener("click", addTodo);
+
+function clearInput() {
+    todoInput.value = '';
+}
+
 function addTodo() {
-    const todoInput = document.getElementById('todoInput');
     const todoText = todoInput.value.trim(); 
     if (todoText) {
         currentList.toDos.push({ task: todoText, completed: false });
-        todoInput.value = '';
+        clearInput();
         render();
     }
 }
-document.getElementById("addButton").addEventListener("click", addTodo);
 
 function toggleTodoCompletion(index) {
     currentList.toDos[index].completed = !currentList.toDos[index].completed;
     render();
 }
 
+function removeTodo(index) {
+    currentList.toDos.splice(index, 1);
+    render();
+}
+
+function markTodoAsCompleted(index) {
+    currentList.toDos[index].completed = !currentList.toDos[index].completed;
+    render();
+}
+
+function removeAllToDosCompleted() {
+    currentList.toDos = currentList.toDos.filter(todo => !todo.completed);
+    render();
+}
+
+function addList() {
+    const listName = prompt("Enter the name of the new list:");
+    if (listName) {
+        const newListId = Object.keys(lists).length + 1;
+        lists[newListId] = { name: listName, toDos: [] };
+        currentList = lists[newListId]; 
+        render();
+    }
+}
+
+function removeList(listId) {
+    if (confirm(`Are you sure you want to delete the list: ${lists[listId].name}?`)) {
+        delete lists[listId];
+        currentList = lists[Object.keys(lists)[0]] || null; 
+        render();
+    }
+}
+
 function render() {
+    
     let listsHtml = '<ul class="list-group">';
     Object.keys(lists).forEach((key) => {
-      listsHtml += `<li class="list-group-item">${lists[key].name}</li>`;
+        listsHtml += `<li class="list-group-item">${lists[key].name}
+                        <button onclick="removeList(${key})">Delete</button>
+                      </li>`;
     });
     listsHtml += '</ul>';
-    document.getElementById('lists').innerHTML = listsHtml;
+    listsContainer.innerHTML = listsHtml;
 
-    document.getElementById('current-list-name').innerText = currentList.name;
+    currentListName.innerText = currentList.name;
 
     let toDosHtml = '<ul class="list-group">';
     currentList.toDos.forEach((todo, index) => {
         toDosHtml += `
             <li class="list-group-item">
-                <input type="checkbox" ${todo.completed ? 'checked' : ''} onclick="toggleTodoCompletion(${index})">
+                <input type="checkbox" ${todo.completed ? 'checked' : ''} onclick="markTodoAsCompleted(${index})">
                 ${todo.task}
+                <button onclick="removeTodo(${index})">Delete</button>
             </li>`;
     });
     toDosHtml += '</ul>';
-    document.getElementById('current-list-toDos').innerHTML = toDosHtml;
+    currentListToDos.innerHTML = toDosHtml;
 }
-
-function addList() {
-    const listName = prompt("Enter the name of the new list:");
-    
-    if (listName) {
-        const newListId = Object.keys(lists).length + 1;
-        lists[newListId] = {
-            name: listName,
-            toDos: []
-        };
-        currentList = lists[newListId]; 
-        render();
-    }
-}
+render();
